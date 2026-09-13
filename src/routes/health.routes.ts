40dc690b5621875b7c1cd8740925821df0934766
Joseph <josephqaz5131@gmail.com>
2026-09-13 15:05:36 -0500

    Revert "Merge pull request #1 from Josephqaz/feature/read-notes"
    This reverts commit 44372b0fc7c8cb188fd9140f7a2a4665b8bdb2ff, reversing
    changes made to 9fec397f5d93c1b2f5bb6e91202fe683dbf323a3.

--- src/routes/health.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-export const healthRouter = Router();
-
-healthRouter.get("/health", (req, res) => {
-    res.json ({ status: "ok",
-    environment: process.env.APP_ENV || "development",
-});
-});
